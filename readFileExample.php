7b4058290b3c7a8d62ce6527adf79a250d06b08c
Abdul Moid <abdulmoidkhan221@gmail.com>
2024-09-01 23:22:20 +0530

    read file examples

---
+++ readFileExample.php
@@ -0,0 +1,12 @@
+<?php
+
+
+// readfile("myfile.txt");
+// Reads a file and writes it to the output buffer.
+// Returns the number of bytes read from the file on success, or false on failure
+
+
+// $read = readfile("myfile.txt");
+// echo $read;
+
+readfile("file.html");
